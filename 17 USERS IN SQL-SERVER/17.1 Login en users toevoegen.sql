c35cf546e01a82ee355d0c8a5b54c70d641e1d18
Fabian Neeleman <101504@glr.nl>
2026-03-13 08:01:58 +0000

    IT Tech Aut

--- 17 USERS IN SQL-SERVER/17.1 Login en users toevoegen.sql
+++ 17 USERS IN SQL-SERVER/17.1 Login en users toevoegen.sql
@@ -2,25 +2,26 @@
 -- Opdracht 17.1
 
 -- Windows users toevoegen \ Lezen
-CREATE LOGIN [SCHOOL\Student1] FROM WINDOWS
+CREATE LOGIN [SQLSRV-101504\Student1] FROM WINDOWS
 USE GameTracker;
-CREATE USER [Student1] FOR LOGIN [SCHOOL\Student1]
+CREATE USER [Student1] FOR LOGIN [SQLSRV-101504\Student1]
 EXEC sp_addrolemember 'db_datareader', 'Student1'
 
 -- Windows users toevoegen \ Lezen, bewerken
-CREATE LOGIN [SCHOOL\Student2] FROM WINDOWS
+CREATE LOGIN [SQLSRV-101504\Student2] FROM WINDOWS
 USE GameTracker;
-CREATE USER [Student2] FOR LOGIN [SCHOOL\Student2]
+CREATE USER [Student2] FOR LOGIN [SQLSRV-101504\Student2]
 EXEC sp_addrolemember 'db_datareader', 'Student2'
 EXEC sp_addrolemember 'db_datawriter', 'Student2'
 
 
--- SQL-users toevoegen
+-- SQL-users toevoegen \ Lezen
 CREATE LOGIN Student3 WITH PASSWORD = '#1Geheim'
 USE GameTracker
 CREATE USER [Student3] FOR LOGIN Student3
 EXEC sp_addrolemember 'db_datareader', 'Student3'
 
+-- SQL-users toevoegen \ Lezen, bewerken
 CREATE LOGIN Student4 WITH PASSWORD = '#1Geheim'
 USE GameTracker
 CREATE USER [Student4] FOR LOGIN Student4
